725986b570af560da6b2122357a40abc5f501600
alex <karpen_ko@hotmail.com>
2020-07-09 19:21:36 +0200

    - added sequential primary key to sync.tbl_sap_stundensatz - added index on mitarbeiter_uid of sync.tbl_sap_stundensatz

--- sql/1/002_tbl_sap_stundensatz.sql
+++ sql/1/002_tbl_sap_stundensatz.sql
@@ -1,4 +1,14 @@
+CREATE SEQUENCE IF NOT EXISTS sync.tbl_sap_stundensatz_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MAXVALUE
+    NO MINVALUE
+    CACHE 1;
+
+GRANT SELECT, UPDATE ON SEQUENCE sync.tbl_sap_stundensatz_id_seq TO vilesci;
+
 CREATE TABLE IF NOT EXISTS sync.tbl_sap_stundensatz (
+    sap_stundensatz_id bigint NOT NULL DEFAULT NEXTVAL('sync.tbl_sap_stundensatz_id_seq'::regclass),
 	mitarbeiter_uid varchar(32) NOT NULL,
 	sap_kalkulatorischer_stundensatz numeric(6,2),
 	insertamum timestamp DEFAULT now()
@@ -10,9 +20,21 @@ COMMENT ON COLUMN sync.tbl_sap_stundensatz.sap_kalkulatorischer_stundensatz IS '
 
 DO $$
 BEGIN
+    ALTER TABLE sync.tbl_sap_stundensatz ADD CONSTRAINT tbl_sap_stundensatz_pkey PRIMARY KEY (sap_stundensatz_id);
+    EXCEPTION WHEN OTHERS THEN NULL;
+END $$;
+
+DO $$
+BEGIN
 	ALTER TABLE ONLY sync.tbl_sap_stundensatz ADD CONSTRAINT fk_tbl_sap_stundensatz_mitarbeiter_uid
 	FOREIGN KEY (mitarbeiter_uid) REFERENCES public.tbl_mitarbeiter(mitarbeiter_uid) ON UPDATE CASCADE ON DELETE RESTRICT;
 	EXCEPTION WHEN OTHERS THEN NULL;
+END $$;
+
+DO $$
+    BEGIN
+        CREATE INDEX idx_tbl_sap_stundensatz_mitarbeiter_uid ON sync.tbl_sap_stundensatz USING btree (mitarbeiter_uid);
+    EXCEPTION WHEN OTHERS THEN NULL;
 END $$;
 
 GRANT SELECT,INSERT,DELETE,UPDATE ON TABLE sync.tbl_sap_stundensatz TO vilesci;
